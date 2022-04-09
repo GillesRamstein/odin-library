7e79a2a50a891ea24bb4f4841a572358875bd61a
Gilles Ramstein <gilles187@hotmail.com>
2022-04-09 04:58:47 +0200

    add new book functionality

--- script.js
+++ script.js
@@ -9,35 +9,45 @@ function Book(title, author, pages, read) {
     this.id = [title, author, pages].join("-").replaceAll(" ", "-");
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+document
+    .getElementById("new-book-button")
+    .addEventListener("click", showNewBookInput);
+
+function showNewBookInput(e) {
+    let form = document.getElementById("form-wrapper");
+    form.classList.toggle("hidden");
 }
 
-function deleteBookFromLibrary(e) {
-    console.log("DELETE BOOK " + this.parentNode.dataset.id);
-    let idx = myLibrary.findIndex(
-        (book) => book.id === this.parentNode.dataset.id
+document.getElementById("form-submit").addEventListener("click", addNewBook);
+
+function addNewBook(e) {
+    addNewBookToDOM(
+        new Book(
+            this.parentNode.querySelector("#input-title").value,
+            this.parentNode.querySelector("#input-author").value,
+            this.parentNode.querySelector("#input-pages").value,
+            1
+        )
     );
-    console.log(idx);
-    myLibrary.splice(idx, 1);
-    console.log(myLibrary);
-    console.log('[data-id="' + this.parentNode.dataset.id + '"]');
-    document
-        .querySelector('[data-id="' + this.parentNode.dataset.id + '"]')
-        .parentNode.remove();
+    hideNewBookInput(e);
 }
 
-function toggleReadBookStatus(e) {
-    console.log("TOGGLE READ");
-    if (this.dataset.read === "false") {
-        this.dataset.read = "true";
-    } else {
-        this.dataset.read = "false";
-    }
-    let book = this.parentNode.parentNode
-    book.querySelector(".book-read").innerText =
-        formatRead(this.dataset.read);
-    book.classList.toggle("read")
+function addNewBookToDOM(book) {
+    let newBook = document.getElementById("book-template").cloneNode(true);
+    newBook.removeAttribute("id");
+    newBook.querySelector(".book-title").textContent = book.title;
+    newBook.querySelector(".book-author").textContent = book.author;
+    newBook.querySelector(".book-pages").textContent = formatPages(book.pages);
+    newBook.querySelector(".book-read").textContent = formatRead(book.read);
+    newBook.classList.remove("hidden");
+    newBook.querySelector(".book-buttons").dataset.id = book.id;
+    newBook
+        .querySelector(".book-buttons .delete")
+        .addEventListener("click", removeBookFromLibrary);
+    newBook
+        .querySelector(".book-buttons .toggle")
+        .addEventListener("click", toggleReadBookStatus);
+    books.insertBefore(newBook, document.getElementById("add-book"));
 }
 
 function formatPages(pages) {
@@ -52,24 +62,38 @@ function formatRead(read) {
     return "Not read yet";
 }
 
-function addBookToDOM(book, i) {
-    let newBook = document.getElementById("book-template").cloneNode(true);
-    newBook.classList.remove("hidden");
-    newBook.removeAttribute("id");
-    // newBook.dataset.id = book.id;
-    newBook.querySelector(".book-title").textContent = book.title;
-    newBook.querySelector(".book-author").textContent = book.author;
-    newBook.querySelector(".book-pages").textContent = formatPages(book.pages);
-    newBook.querySelector(".book-read").textContent = formatRead(book.read);
-    newBook.querySelector(".book-buttons").dataset.id = book.id;
-    newBook
-        .querySelector(".book-buttons .delete")
-        .addEventListener("click", deleteBookFromLibrary);
-    newBook
-        .querySelector(".book-buttons .toggle")
-        .addEventListener("click", toggleReadBookStatus);
-    books.insertBefore(newBook, document.getElementById("add-book"));
+function removeBookFromLibrary(e) {
+    let idx = myLibrary.findIndex(
+        (book) => book.id === this.parentNode.dataset.id
+    );
+    myLibrary.splice(idx, 1);
+    removeBookFromDOM(this.parentNode);
 }
+
+function removeBookFromDOM(book) {
+    document
+        .querySelector('[data-id="' + book.dataset.id + '"]')
+        .parentNode.remove();
+}
+
+function toggleReadBookStatus(e) {
+    console.log("TOGGLE READ");
+    if (this.dataset.read === "false") {
+        this.dataset.read = "true";
+    } else {
+        this.dataset.read = "false";
+    }
+    let book = this.parentNode.parentNode;
+    book.querySelector(".book-read").innerText = formatRead(this.dataset.read);
+    book.classList.toggle("read");
+}
+
+function hideNewBookInput(e) {
+    let form = document.getElementById("form-wrapper");
+    form.classList.toggle("hidden");
+}
+
+// Populate library with dummy books
 
 myLibrary = [
     new Book("Book 1", "Author 1", 1, false),
@@ -77,8 +101,4 @@ myLibrary = [
     new Book("Book 3", "Author 1", 1, false),
     new Book("Book 4", "Author 2", 2, true),
 ];
-
-console.log(myLibrary);
-
-// myLibrary.forEach()
-myLibrary.forEach((book, i) => addBookToDOM(book, i));
+myLibrary.forEach((book) => addNewBookToDOM(book));
