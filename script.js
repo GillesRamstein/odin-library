b7ed2e0c7cefc0a7f8b92c085d1b96638b212319
Gilles Ramstein <gilles187@hotmail.com>
2022-04-09 02:54:22 +0200

    delete books, toggle read

--- script.js
+++ script.js
@@ -6,10 +6,38 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = [title, author, pages].join("-").replaceAll(" ", "-");
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+function deleteBookFromLibrary(e) {
+    console.log("DELETE BOOK " + this.parentNode.dataset.id);
+    let idx = myLibrary.findIndex(
+        (book) => book.id === this.parentNode.dataset.id
+    );
+    console.log(idx);
+    myLibrary.splice(idx, 1);
+    console.log(myLibrary);
+    console.log('[data-id="' + this.parentNode.dataset.id + '"]');
+    document
+        .querySelector('[data-id="' + this.parentNode.dataset.id + '"]')
+        .parentNode.remove();
+}
+
+function toggleReadBookStatus(e) {
+    console.log("TOGGLE READ");
+    if (this.dataset.read === "false") {
+        this.dataset.read = "true";
+    } else {
+        this.dataset.read = "false";
+    }
+    let book = this.parentNode.parentNode
+    book.querySelector(".book-read").innerText =
+        formatRead(this.dataset.read);
+    book.classList.toggle("read")
 }
 
 function formatPages(pages) {
@@ -20,23 +48,37 @@ function formatPages(pages) {
 }
 
 function formatRead(read) {
-    if (read) return "Already read";
+    if (read === "true") return "Already read";
     return "Not read yet";
 }
 
-function displayBook(book) {
+function addBookToDOM(book, i) {
     let newBook = document.getElementById("book-template").cloneNode(true);
     newBook.classList.remove("hidden");
-    newBook.querySelector("#book-title").textContent = book.title;
-    newBook.querySelector("#book-author").textContent = book.author;
-    newBook.querySelector("#book-pages").textContent = formatPages(book.pages);
-    newBook.querySelector("#book-read").textContent = formatRead(book.read);
+    newBook.removeAttribute("id");
+    // newBook.dataset.id = book.id;
+    newBook.querySelector(".book-title").textContent = book.title;
+    newBook.querySelector(".book-author").textContent = book.author;
+    newBook.querySelector(".book-pages").textContent = formatPages(book.pages);
+    newBook.querySelector(".book-read").textContent = formatRead(book.read);
+    newBook.querySelector(".book-buttons").dataset.id = book.id;
+    newBook
+        .querySelector(".book-buttons .delete")
+        .addEventListener("click", deleteBookFromLibrary);
+    newBook
+        .querySelector(".book-buttons .toggle")
+        .addEventListener("click", toggleReadBookStatus);
     books.insertBefore(newBook, document.getElementById("add-book"));
 }
 
 myLibrary = [
     new Book("Book 1", "Author 1", 1, false),
     new Book("Book 2", "Author 2", 2, true),
+    new Book("Book 3", "Author 1", 1, false),
+    new Book("Book 4", "Author 2", 2, true),
 ];
 
-myLibrary.forEach((book) => displayBook(book));
+console.log(myLibrary);
+
+// myLibrary.forEach()
+myLibrary.forEach((book, i) => addBookToDOM(book, i));
